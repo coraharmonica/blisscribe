e1057a7c6b9fa48c1787592dae38b1b21d124fb6
Cora <courtneyhermary@gmail.com>
2017-02-28 22:08:56 -0800

    debugged tool for unpacking font glyphs (now working)

--- main/unpack_bliss_font.py
+++ main/unpack_bliss_font.py
@@ -1,8 +1,25 @@
+# NOTES
+# =====
+#
+# Fetch Glyph Address(es)
+# -----------------------
+# Given the first and last glyph addresses in a font family,
+# returns a list of all glyph addresses in the same font family.
+
+
+# CONSTANTS
+# =========
+
 blissFontPath = "/Users/courtney/Library/Fonts/CcfSymbolFont-bliss-2012.ttf"
 firstBlissChar = "\ue00a"
-lastBlissChar = "\ue00f" #f3c0
+lastBlissChar = "\uf3c0"
 
-# TODO: create list of all glyph addresses based on first and last glyph addresses (in hexadecimal)
+
+# FUNCTIONS
+# =========
+
+# helpers
+# -------
 
 def getNextChar(char):
     """
@@ -15,7 +32,12 @@ def getNextChar(char):
     for elt in hex:
         idx += 1
         if elt == char:
-            return hex[idx]
+            if elt == hex[-1]:
+                return hex[0]
+            else:
+                return hex[idx]
+
+    return ""
 
 
 def reverse(string):
@@ -26,53 +48,53 @@ def reverse(string):
     new = ""
 
     for char in string:
-        new = string + new
+        new = char + new
 
     return new
 
+
+# main
+# ----
 
 def getNextGlyphAddress(prev):
     """
     :param prev: [str] preceding glyph address (in hex)
     :return: [str] next glyph address (in hex)
     """
-    new = "\u"
-    abcs = "abcdef"
-    nums = "0123456789"
-
-    revPrev = reverse(prev)
+    new = ""
+    revPrev = reverse(prev) # reversed for ease of addition (+)
     idx = 0
+    changeNext = False
 
     for char in revPrev:
-        if idx == 0:
-            new += getNextChar(prev[-1])
+        if idx == 0 or changeNext == True:
+            new += getNextChar(char)
             if char == "f":
-                new += getNextChar(prev[-2])
+                changeNext = True
+            else:
+                changeNext = False
         else:
             new += char
+        idx += 1
 
-    return new
+    return reverse(new)
 
 
-def getGlyphAddresses(first, last):
+def getNextGlyphAddresses(first, last):
     """
     :param first: [str] first glyph address in a given font set
     :param last: [str] last glyph address in (same) font set
     :return: [list] all glyph addresses in a font set
     """
     addresses = [first]
-    rsf = []
-
     current = first
     goal = last
 
     while current != goal:
-        getNextGlyphAddress(current)
-
-    for address in rsf:
-        addresses.append("\u" + address)
+        current = getNextGlyphAddress(current)
+        addresses.append(current)
 
     return addresses
 
 
-getGlyphAddresses(firstBlissChar, lastBlissChar)
+#print getNextGlyphAddresses(firstBlissChar, lastBlissChar)
