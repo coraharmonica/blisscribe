06e7374a3db8f565a1e8ccb5644515908845351f
Cora <courtneyhermary@gmail.com>
2017-11-17 16:35:52 -0800

    integrating local w/ remote, renaming projects to main

--- projects/bliss_web/bliss_app/bliss_webapp/blisscribe_py/tests.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.test import TestCase
-import blisscribe_py.blisscribe
-
-LEX_PARSER = blisscribe_py.blisscribe.LexiconParser()
-LEX_PARSER.readILIMapping()
--- projects/bliss_web/bliss_app/bliss_webapp/tests.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.test import TestCase
-import blisscribe_py.parse_lexica
-
-
-LEX_PARSER = blisscribe_py.parse_lexica.LexiconParser()
-LEX_PARSER.readILIMapping()
