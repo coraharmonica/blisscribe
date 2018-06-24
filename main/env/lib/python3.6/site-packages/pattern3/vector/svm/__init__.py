f464b707d8de638152d1ad12b82b8ef526e735d9
Cora <courtneyhermary@gmail.com>
2018-06-23 17:18:20 -0700

    update to python 3.6

---
+++ main/env/lib/python3.6/site-packages/pattern3/vector/svm/__init__.py
@@ -0,0 +1,15 @@
+
+LIBSVM = LIBLINEAR = True
+
+try:
+    from . import libsvm
+    from . import libsvmutil
+except ImportError as e:
+    LIBSVM = False
+    raise e
+
+try:
+    from . import liblinear
+    from . import liblinearutil
+except:
+    LIBLINEAR = False
